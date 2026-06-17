ac2fe4527f27165a4ab2bce5777d610e76ba3179
Jean Machuca <jeanmachuca@users.noreply.github.com>
2026-06-16 22:44:53 -0400

    Delete reset.sh

--- reset.sh
+++
@@ -1,7 +0,0 @@
-rm -Rf /Users/jeanmachuca/.code-inference    
-rm -Rf .opencode/node_modules 
-rm .opencode/package.json
-rm .opencode/package-lock.json
-rm .git/opencode
-curl -sS https://raw.githubusercontent.com/jeanmachuca/code-inference/main/install.sh | sh      
-docker compose -f  /Users/jeanmachuca/.code-inference/docker-compose.yml --profile stack down
